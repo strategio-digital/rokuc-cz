a94f60ad2be8fc97dbaa9bcf917036a803c822d2
Jiří Zapletal <jz@strategio.digital>
2022-04-10 22:51:23 +0200

    Refactored lazy loading

--- assets/typescript/components/LazyThumb.ts
+++ assets/typescript/components/LazyThumb.ts
@@ -15,35 +15,44 @@ export default () => {
         const data = JSON.parse(params);
         const suffix = '--' + data.method.toUpperCase() + '-' + data.quality + '-' + (data.size.width ?? '') + 'x' + (data.size.height ?? '');
 
+        // DEV
+        //return 'https://dev-contentio-app.s3.eu-west-1.amazonaws.com/_develop_contentio_app/article/1/test--EXACT-80-200x200.jpg';
+        //return 'https://cdn.contentio.dev/_develop_contentio_app/article/1/test--EXACT-80-200x200.jpg';
+        //return 'https://dg8jgypxflvcm.cloudfront.net/_develop_contentio_app/article/1/test--EXACT-80-200x200.jpg';
+
+        // PROD
+        //return 'https://contentio-app.s3.eu-west-1.amazonaws.com/prod_rokuc_cz/article/9/03-Zjistovani-vlhkosti-v-plochych-strechach.png';
+        //return 'https://cdn.contentio.app/prod_rokuc_cz/article/9/03-Zjistovani-vlhkosti-v-plochych-strechach.png';
+        //return 'https://d13wjlmuheewzx.cloudfront.net/prod_rokuc_cz/article/9/03-Zjistovani-vlhkosti-v-plochych-strechach.png';
+
         return process.env.CDN_ENDPOINT + path + basename + suffix + extension;
     }
 
     document.querySelectorAll('[data-img-path]').forEach(async node => {
+
+        // Todo: loading icon
+        // Todo: not found icon
         const image = node as HTMLImageElement;
 
         if (image.dataset.imgPath && image.dataset.imgParams) {
-            const params = JSON.parse(image.dataset.imgParams);
-            const url = createUrl(image.dataset.imgPath, image.dataset.imgParams);
+            let cdnResponse = null;
 
             try {
-                // Todo: fix cors
-                const cdnResponse = await Axios.get(url, {responseType: 'blob' });
-                if (cdnResponse.status === 200) {
-                    const reader = new FileReader();
-                    reader.readAsDataURL(cdnResponse.data);
-                    reader.onload = () => {
-                        image.src = reader.result as string;
-                    };
-                }
+                const url = createUrl(image.dataset.imgPath, image.dataset.imgParams);
+                cdnResponse = await Axios.get(url, {responseType: 'blob'});
 
-                return 1;
-            } catch (e) {}
+                const reader = new FileReader();
+                reader.readAsDataURL(cdnResponse.data);
+                reader.onload = () => image.src = reader.result as string;
 
-            try {
+            } catch (error: any) {
+                cdnResponse = error.response;
+            }
+
+            if (cdnResponse.status === 403) {
+                const params = JSON.parse(image.dataset.imgParams);
                 const apiResponse = await Axios.post('image/create', {path: image.dataset.imgPath, ...params});
                 image.src = apiResponse.data.base64;
-            } catch (e: any) {
-                console.error(node, e.response.data.messages);
             }
         }
     });
